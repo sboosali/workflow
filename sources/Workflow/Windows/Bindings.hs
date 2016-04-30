dec4efd576effcd8cff1338aa177ac93465e60fb
Spiros Boosalis <samboosalis@gmail.com>
2016-04-30 09:52:10 +0300

    all VK

--- sources/Workflow/Windows/Bindings.hs
+++ sources/Workflow/Windows/Bindings.hs
@@ -124,239 +124,231 @@ virtual key codes from @<WinUser.h>@.
 
 -}
 vkKeyboard :: Keyboard VK
-vkKeyboard = Keyboard{..}
- where
+vkKeyboard = Keyboard{..} where
  fromKey = \case
 
- {-
-  #define VK_BACK           0x08
-  #define VK_TAB            0x09
+    VK_BACK           -> 0x08
+    VK_TAB            -> 0x09
 
-  #define VK_CLEAR          0x0C
-  #define VK_RETURN         0x0D
+    VK_CLEAR          -> 0x0C
+    VK_RETURN         -> 0x0D
+    VK_SHIFT          -> 0x10
+    VK_CONTROL        -> 0x11
 
-  #define VK_SHIFT          0x10
-  -}
-   VK_CONTROL -> 0x11
-  {-
-  #define VK_MENU           0x12
-  #define VK_PAUSE          0x13
-  #define VK_CAPITAL        0x14
+    VK_MENU           -> 0x12
+    VK_PAUSE          -> 0x13
+    VK_CAPITAL        -> 0x14
 
-  #define VK_KANA           0x15
-  #define VK_HANGEUL        0x15  -- old name - should be here for compatibility
-  #define VK_HANGUL         0x15
-  #define VK_JUNJA          0x17
-  #define VK_FINAL          0x18
-  #define VK_HANJA          0x19
-  #define VK_KANJI          0x19
+    VK_KANA           -> 0x15
+    VK_HANGEUL        -> 0x15  -- old name - should be here for compatibility
+    VK_HANGUL         -> 0x15
+    VK_JUNJA          -> 0x17
+    VK_FINAL          -> 0x18
+    VK_HANJA          -> 0x19
+    VK_KANJI          -> 0x19
+    VK_ESCAPE         -> 0x1B
 
-  #define VK_ESCAPE         0x1B
+    VK_CONVERT        -> 0x1C
+    VK_NONCONVERT     -> 0x1D
+    VK_ACCEPT         -> 0x1E
+    VK_MODECHANGE     -> 0x1F
 
-  #define VK_CONVERT        0x1C
-  #define VK_NONCONVERT     0x1D
-  #define VK_ACCEPT         0x1E
-  #define VK_MODECHANGE     0x1F
+    VK_SPACE          -> 0x20
+    VK_PRIOR          -> 0x21
+    VK_NEXT           -> 0x22
+    VK_END            -> 0x23
+    VK_HOME           -> 0x24
+    VK_LEFT           -> 0x25
+    VK_UP             -> 0x26
+    VK_RIGHT          -> 0x27
+    VK_DOWN           -> 0x28
+    VK_SELECT         -> 0x29
+    VK_PRINT          -> 0x2A
+    VK_EXECUTE        -> 0x2B
+    VK_SNAPSHOT       -> 0x2C
+    VK_INSERT         -> 0x2D
+    VK_DELETE         -> 0x2E
+    VK_HELP           -> 0x2F
 
-  #define VK_SPACE          0x20
-  #define VK_PRIOR          0x21
-  #define VK_NEXT           0x22
-  #define VK_END            0x23
-  #define VK_HOME           0x24
-  #define VK_LEFT           0x25
-  #define VK_UP             0x26
-  #define VK_RIGHT          0x27
-  #define VK_DOWN           0x28
-  #define VK_SELECT         0x29
-  #define VK_PRINT          0x2A
-  #define VK_EXECUTE        0x2B
-  #define VK_SNAPSHOT       0x2C
-  #define VK_INSERT         0x2D
-  #define VK_DELETE         0x2E
-  #define VK_HELP           0x2F
- -}
+    -- VK_0 - VK_9 are the same as ASCII '0' - '9' (0x30 - 0x39)
 
-   -- VK_0 - VK_9 are the same as ASCII '0' - '9' (0x30 - 0x39)
+    VK_0              -> 0x30
+    VK_1              -> 0x31
+    VK_2              -> 0x32
+    VK_3              -> 0x33
+    VK_4              -> 0x34
+    VK_5              -> 0x35
+    VK_6              -> 0x36
+    VK_7              -> 0x37
+    VK_8              -> 0x38
+    VK_9              -> 0x39
 
-   VK_0 -> 0x30
-   VK_1 -> 0x31
-   VK_2 -> 0x32
-   VK_3 -> 0x33
-   VK_4 -> 0x34
-   VK_5 -> 0x35
-   VK_6 -> 0x36
-   VK_7 -> 0x37
-   VK_8 -> 0x38
-   VK_9 -> 0x39
+    -- 0x40 : unassigned
+    -- VK_A - VK_Z are the same as ASCII 'A' - 'Z' (0x41 - 0x5A)
+    VK_A              -> 0x41
+    VK_B              -> 0x42
+    VK_C              -> 0x43
+    VK_D              -> 0x44
+    VK_E              -> 0x45
+    VK_F              -> 0x46
+    VK_G              -> 0x47
+    VK_H              -> 0x48
+    VK_I              -> 0x49
+    VK_J              -> 0x4A
+    VK_K              -> 0x4B
+    VK_L              -> 0x4C
+    VK_M              -> 0x4D
+    VK_N              -> 0x4E
+    VK_O              -> 0x4F
+    VK_P              -> 0x50
+    VK_Q              -> 0x51
+    VK_R              -> 0x52
+    VK_S              -> 0x53
+    VK_T              -> 0x54
+    VK_U              -> 0x55
+    VK_V              -> 0x56
+    VK_W              -> 0x57
+    VK_X              -> 0x58
+    VK_Y              -> 0x59
+    VK_Z              -> 0x5A
 
-   -- 0x40 : unassigned
-   -- VK_A - VK_Z are the same as ASCII 'A' - 'Z' (0x41 - 0x5A)
-   VK_A -> 0x41
-   VK_B -> 0x42
-   VK_C -> 0x43
-   VK_D -> 0x44
-   VK_E -> 0x45
-   VK_F -> 0x46
-   VK_G -> 0x47
-   VK_H -> 0x48
-   VK_I -> 0x49
-   VK_J -> 0x4A
-   VK_K -> 0x4B
-   VK_L -> 0x4C
-   VK_M -> 0x4D
-   VK_N -> 0x4E
-   VK_O -> 0x4F
-   VK_P -> 0x50
-   VK_Q -> 0x51
-   VK_R -> 0x52
-   VK_S -> 0x53
-   VK_T -> 0x54
-   VK_U -> 0x55
-   VK_V -> 0x56
-   VK_W -> 0x57
-   VK_X -> 0x58
-   VK_Y -> 0x59
-   VK_Z -> 0x5A
+    VK_LWIN           -> 0x5B
+    VK_RWIN           -> 0x5C
+    VK_APPS           -> 0x5D
 
- {-
-  #define VK_LWIN           0x5B
-  #define VK_RWIN           0x5C
-  #define VK_APPS           0x5D
+    VK_SLEEP          -> 0x5F
 
-  #define VK_SLEEP          0x5F
+    VK_NUMPAD0        -> 0x60
+    VK_NUMPAD1        -> 0x61
+    VK_NUMPAD2        -> 0x62
+    VK_NUMPAD3        -> 0x63
+    VK_NUMPAD4        -> 0x64
+    VK_NUMPAD5        -> 0x65
+    VK_NUMPAD6        -> 0x66
+    VK_NUMPAD7        -> 0x67
+    VK_NUMPAD8        -> 0x68
+    VK_NUMPAD9        -> 0x69
+    VK_MULTIPLY       -> 0x6A
+    VK_ADD            -> 0x6B
+    VK_SEPARATOR      -> 0x6C
+    VK_SUBTRACT       -> 0x6D
+    VK_DECIMAL        -> 0x6E
+    VK_DIVIDE         -> 0x6F
+    VK_F1             -> 0x70
+    VK_F2             -> 0x71
+    VK_F3             -> 0x72
+    VK_F4             -> 0x73
+    VK_F5             -> 0x74
+    VK_F6             -> 0x75
+    VK_F7             -> 0x76
+    VK_F8             -> 0x77
+    VK_F9             -> 0x78
+    VK_F10            -> 0x79
+    VK_F11            -> 0x7A
+    VK_F12            -> 0x7B
+    VK_F13            -> 0x7C
+    VK_F14            -> 0x7D
+    VK_F15            -> 0x7E
+    VK_F16            -> 0x7F
+    VK_F17            -> 0x80
+    VK_F18            -> 0x81
+    VK_F19            -> 0x82
+    VK_F20            -> 0x83
+    VK_F21            -> 0x84
+    VK_F22            -> 0x85
+    VK_F23            -> 0x86
+    VK_F24            -> 0x87
 
-  #define VK_NUMPAD0        0x60
-  #define VK_NUMPAD1        0x61
-  #define VK_NUMPAD2        0x62
-  #define VK_NUMPAD3        0x63
-  #define VK_NUMPAD4        0x64
-  #define VK_NUMPAD5        0x65
-  #define VK_NUMPAD6        0x66
-  #define VK_NUMPAD7        0x67
-  #define VK_NUMPAD8        0x68
-  #define VK_NUMPAD9        0x69
-  #define VK_MULTIPLY       0x6A
-  #define VK_ADD            0x6B
-  #define VK_SEPARATOR      0x6C
-  #define VK_SUBTRACT       0x6D
-  #define VK_DECIMAL        0x6E
-  #define VK_DIVIDE         0x6F
-  #define VK_F1             0x70
-  #define VK_F2             0x71
-  #define VK_F3             0x72
-  #define VK_F4             0x73
-  #define VK_F5             0x74
-  #define VK_F6             0x75
-  #define VK_F7             0x76
-  #define VK_F8             0x77
-  #define VK_F9             0x78
-  #define VK_F10            0x79
-  #define VK_F11            0x7A
-  #define VK_F12            0x7B
-  #define VK_F13            0x7C
-  #define VK_F14            0x7D
-  #define VK_F15            0x7E
-  #define VK_F16            0x7F
-  #define VK_F17            0x80
-  #define VK_F18            0x81
-  #define VK_F19            0x82
-  #define VK_F20            0x83
-  #define VK_F21            0x84
-  #define VK_F22            0x85
-  #define VK_F23            0x86
-  #define VK_F24            0x87
+    VK_NUMLOCK        -> 0x90
+    VK_SCROLL         -> 0x91
 
-  #define VK_NUMLOCK        0x90
-  #define VK_SCROLL         0x91
+    -- NEC PC-9800 kbd definitions
+    VK_OEM_NEC_EQUAL  -> 0x92   -- '=' key on numpad
 
-  -- NEC PC-9800 kbd definitions
-  #define VK_OEM_NEC_EQUAL  0x92   -- '=' key on numpad
+    -- Fujitsu/OASYS kbd definitions
+    VK_OEM_FJ_JISHO   -> 0x92   -- 'Dictionary' key
+    VK_OEM_FJ_MASSHOU -> 0x93   -- 'Unregister word' key
+    VK_OEM_FJ_TOUROKU -> 0x94   -- 'Register word' key
+    VK_OEM_FJ_LOYA    -> 0x95   -- 'Left OYAYUBI' key
+    VK_OEM_FJ_ROYA    -> 0x96   -- 'Right OYAYUBI' key
 
-  -- Fujitsu/OASYS kbd definitions
-  #define VK_OEM_FJ_JISHO   0x92   -- 'Dictionary' key
-  #define VK_OEM_FJ_MASSHOU 0x93   -- 'Unregister word' key
-  #define VK_OEM_FJ_TOUROKU 0x94   -- 'Register word' key
-  #define VK_OEM_FJ_LOYA    0x95   -- 'Left OYAYUBI' key
-  #define VK_OEM_FJ_ROYA    0x96   -- 'Right OYAYUBI' key
+    -- VK_L* & VK_R* - left and right Alt, Ctrl and Shift virtual keys.
+    -- Used only as parameters to GetAsyncKeyState() and GetKeyState().
+    -- No other API or message will distinguish left and right keys in this way.
 
-   -- VK_L* & VK_R* - left and right Alt, Ctrl and Shift virtual keys.
-   -- Used only as parameters to GetAsyncKeyState() and GetKeyState().
-   -- No other API or message will distinguish left and right keys in this way.
+    VK_LSHIFT         -> 0xA0
+    VK_RSHIFT         -> 0xA1
+    VK_LCONTROL       -> 0xA2
+    VK_RCONTROL       -> 0xA3
+    VK_LMENU          -> 0xA4
+    VK_RMENU          -> 0xA5
 
-  #define VK_LSHIFT         0xA0
-  #define VK_RSHIFT         0xA1
-  #define VK_LCONTROL       0xA2
-  #define VK_RCONTROL       0xA3
-  #define VK_LMENU          0xA4
-  #define VK_RMENU          0xA5
+    VK_BROWSER_BACK        -> 0xA6
+    VK_BROWSER_FORWARD     -> 0xA7
+    VK_BROWSER_REFRESH     -> 0xA8
+    VK_BROWSER_STOP        -> 0xA9
+    VK_BROWSER_SEARCH      -> 0xAA
+    VK_BROWSER_FAVORITES   -> 0xAB
+    VK_BROWSER_HOME        -> 0xAC
 
-  #define VK_BROWSER_BACK        0xA6
-  #define VK_BROWSER_FORWARD     0xA7
-  #define VK_BROWSER_REFRESH     0xA8
-  #define VK_BROWSER_STOP        0xA9
-  #define VK_BROWSER_SEARCH      0xAA
-  #define VK_BROWSER_FAVORITES   0xAB
-  #define VK_BROWSER_HOME        0xAC
+    VK_VOLUME_MUTE         -> 0xAD
+    VK_VOLUME_DOWN         -> 0xAE
+    VK_VOLUME_UP           -> 0xAF
+    VK_MEDIA_NEXT_TRACK    -> 0xB0
+    VK_MEDIA_PREV_TRACK    -> 0xB1
+    VK_MEDIA_STOP          -> 0xB2
+    VK_MEDIA_PLAY_PAUSE    -> 0xB3
+    VK_LAUNCH_MAIL         -> 0xB4
+    VK_LAUNCH_MEDIA_SELECT -> 0xB5
+    VK_LAUNCH_APP1         -> 0xB6
+    VK_LAUNCH_APP2         -> 0xB7
 
-  #define VK_VOLUME_MUTE         0xAD
-  #define VK_VOLUME_DOWN         0xAE
-  #define VK_VOLUME_UP           0xAF
-  #define VK_MEDIA_NEXT_TRACK    0xB0
-  #define VK_MEDIA_PREV_TRACK    0xB1
-  #define VK_MEDIA_STOP          0xB2
-  #define VK_MEDIA_PLAY_PAUSE    0xB3
-  #define VK_LAUNCH_MAIL         0xB4
-  #define VK_LAUNCH_MEDIA_SELECT 0xB5
-  #define VK_LAUNCH_APP1         0xB6
-  #define VK_LAUNCH_APP2         0xB7
+    VK_OEM_1          -> 0xBA   -- ';:' for US
+    VK_OEM_PLUS       -> 0xBB   -- '+' any country
+    VK_OEM_COMMA      -> 0xBC   -- ',' any country
+    VK_OEM_MINUS      -> 0xBD   -- '-' any country
+    VK_OEM_PERIOD     -> 0xBE   -- '.' any country
+    VK_OEM_2          -> 0xBF   -- '/?' for US
+    VK_OEM_3          -> 0xC0   -- '`~' for US
 
-  #define VK_OEM_1          0xBA   -- ';:' for US
-  #define VK_OEM_PLUS       0xBB   -- '+' any country
-  #define VK_OEM_COMMA      0xBC   -- ',' any country
-  #define VK_OEM_MINUS      0xBD   -- '-' any country
-  #define VK_OEM_PERIOD     0xBE   -- '.' any country
-  #define VK_OEM_2          0xBF   -- '/?' for US
-  #define VK_OEM_3          0xC0   -- '`~' for US
+    VK_OEM_4          -> 0xDB  --  '[{' for US
+    VK_OEM_5          -> 0xDC  --  '\|' for US
+    VK_OEM_6          -> 0xDD  --  ']}' for US
+    VK_OEM_7          -> 0xDE  --  ''"' for US
+    VK_OEM_8          -> 0xDF
 
-  #define VK_OEM_4          0xDB  --  '[{' for US
-  #define VK_OEM_5          0xDC  --  '\|' for US
-  #define VK_OEM_6          0xDD  --  ']}' for US
-  #define VK_OEM_7          0xDE  --  ''"' for US
-  #define VK_OEM_8          0xDF
+    -- Various extended or enhanced keyboards
+    VK_OEM_AX         -> 0xE1  --  'AX' key on Japanese AX kbd
+    VK_OEM_102        -> 0xE2  --  "<>" or "\|" on RT 102-key kbd.
+    VK_ICO_HELP       -> 0xE3  --  Help key on ICO
+    VK_ICO_00         -> 0xE4  --  00 key on ICO
 
-  -- Various extended or enhanced keyboards
-  #define VK_OEM_AX         0xE1  --  'AX' key on Japanese AX kbd
-  #define VK_OEM_102        0xE2  --  "<>" or "\|" on RT 102-key kbd.
-  #define VK_ICO_HELP       0xE3  --  Help key on ICO
-  #define VK_ICO_00         0xE4  --  00 key on ICO
+    VK_PROCESSKEY     -> 0xE5
 
-  #define VK_PROCESSKEY     0xE5
+    VK_ICO_CLEAR      -> 0xE6
 
-  #define VK_ICO_CLEAR      0xE6
+    VK_PACKET         -> 0xE7
 
-  #define VK_PACKET         0xE7
+    VK_OEM_RESET      -> 0xE9
+    VK_OEM_JUMP       -> 0xEA
+    VK_OEM_PA1        -> 0xEB
+    VK_OEM_PA2        -> 0xEC
+    VK_OEM_PA3        -> 0xED
+    VK_OEM_WSCTRL     -> 0xEE
+    VK_OEM_CUSEL      -> 0xEF
+    VK_OEM_ATTN       -> 0xF0
+    VK_OEM_FINISH     -> 0xF1
+    VK_OEM_COPY       -> 0xF2
+    VK_OEM_AUTO       -> 0xF3
+    VK_OEM_ENLW       -> 0xF4
+    VK_OEM_BACKTAB    -> 0xF5
 
-  #define VK_OEM_RESET      0xE9
-  #define VK_OEM_JUMP       0xEA
-  #define VK_OEM_PA1        0xEB
-  #define VK_OEM_PA2        0xEC
-  #define VK_OEM_PA3        0xED
-  #define VK_OEM_WSCTRL     0xEE
-  #define VK_OEM_CUSEL      0xEF
-  #define VK_OEM_ATTN       0xF0
-  #define VK_OEM_FINISH     0xF1
-  #define VK_OEM_COPY       0xF2
-  #define VK_OEM_AUTO       0xF3
-  #define VK_OEM_ENLW       0xF4
-  #define VK_OEM_BACKTAB    0xF5
-
-  #define VK_ATTN           0xF6
-  #define VK_CRSEL          0xF7
-  #define VK_EXSEL          0xF8
-  #define VK_EREOF          0xF9
-  #define VK_PLAY           0xFA
-  #define VK_ZOOM           0xFB
-  #define VK_NONAME         0xFC
-  #define VK_PA1            0xFD
-  #define VK_OEM_CLEAR      0xFE
--}
+    VK_ATTN           -> 0xF6
+    VK_CRSEL          -> 0xF7
+    VK_EXSEL          -> 0xF8
+    VK_EREOF          -> 0xF9
+    VK_PLAY           -> 0xFA
+    VK_ZOOM           -> 0xFB
+    VK_NONAME         -> 0xFC
+    VK_PA1            -> 0xFD
+    VK_OEM_CLEAR      -> 0xFE
--- sources/Workflow/Windows/Types.hs
+++ sources/Workflow/Windows/Types.hs
@@ -4,15 +4,18 @@ module Workflow.Windows.Types where
 import Foreign.C.Types
 import Foreign.C.String
 import Data.Word
+import Data.Int
 import GHC.Exts
 
 type LPCWSTR = CWString
 
 type WCHAR_T = CWchar
 
-type UINT = Word32
+type UINT = Word32 -- CUInt
 
-type WORD = Word
+type WORD = Word -- platform-dependent size, in both C and Haskell
+
+type LONG = Int64 -- CLong
 
 {-|
 
@@ -32,44 +35,254 @@ newtype URL = URL String
  deriving (Show, IsString)
 
 data VK
- = VK_CONTROL
 
- | VK_0
- | VK_1
- | VK_2
- | VK_3
- | VK_4
- | VK_5
- | VK_6
- | VK_7
- | VK_8
- | VK_9
+   = VK_BACK           -- ^
+   | VK_TAB            -- ^
 
- | VK_A
- | VK_B
- | VK_C
- | VK_D
- | VK_E
- | VK_F
- | VK_G
- | VK_H
- | VK_I
- | VK_J
- | VK_K
- | VK_L
- | VK_M
- | VK_N
- | VK_O
- | VK_P
- | VK_Q
- | VK_R
- | VK_S
- | VK_T
- | VK_U
- | VK_V
- | VK_W
- | VK_X
- | VK_Y
- | VK_Z
+   | VK_CLEAR          -- ^
+   | VK_RETURN         -- ^
+   | VK_SHIFT          -- ^
+   | VK_CONTROL        -- ^
 
- deriving (Show,Enum)
+   | VK_MENU           -- ^
+   | VK_PAUSE          -- ^
+   | VK_CAPITAL        -- ^
+
+   | VK_KANA           -- ^
+   | VK_HANGEUL        -- ^
+   | VK_HANGUL         -- ^
+   | VK_JUNJA          -- ^
+   | VK_FINAL          -- ^
+   | VK_HANJA          -- ^
+   | VK_KANJI          -- ^
+   | VK_ESCAPE         -- ^
+
+   | VK_CONVERT        -- ^
+   | VK_NONCONVERT     -- ^
+   | VK_ACCEPT         -- ^
+   | VK_MODECHANGE     -- ^
+
+   | VK_SPACE          -- ^
+   | VK_PRIOR          -- ^
+   | VK_NEXT           -- ^
+   | VK_END            -- ^
+   | VK_HOME           -- ^
+   | VK_LEFT           -- ^
+   | VK_UP             -- ^
+   | VK_RIGHT          -- ^
+   | VK_DOWN           -- ^
+   | VK_SELECT         -- ^
+   | VK_PRINT          -- ^
+   | VK_EXECUTE        -- ^
+   | VK_SNAPSHOT       -- ^
+   | VK_INSERT         -- ^
+   | VK_DELETE         -- ^
+   | VK_HELP           -- ^
+
+   | VK_0              -- ^
+   | VK_1              -- ^
+   | VK_2              -- ^
+   | VK_3              -- ^
+   | VK_4              -- ^
+   | VK_5              -- ^
+   | VK_6              -- ^
+   | VK_7              -- ^
+   | VK_8              -- ^
+   | VK_9              -- ^
+
+   | VK_A              -- ^
+   | VK_B              -- ^
+   | VK_C              -- ^
+   | VK_D              -- ^
+   | VK_E              -- ^
+   | VK_F              -- ^
+   | VK_G              -- ^
+   | VK_H              -- ^
+   | VK_I              -- ^
+   | VK_J              -- ^
+   | VK_K              -- ^
+   | VK_L              -- ^
+   | VK_M              -- ^
+   | VK_N              -- ^
+   | VK_O              -- ^
+   | VK_P              -- ^
+   | VK_Q              -- ^
+   | VK_R              -- ^
+   | VK_S              -- ^
+   | VK_T              -- ^
+   | VK_U              -- ^
+   | VK_V              -- ^
+   | VK_W              -- ^
+   | VK_X              -- ^
+   | VK_Y              -- ^
+   | VK_Z              -- ^
+
+   | VK_LWIN           -- ^
+   | VK_RWIN           -- ^
+   | VK_APPS           -- ^
+
+   | VK_SLEEP          -- ^
+
+   | VK_NUMPAD0        -- ^
+   | VK_NUMPAD1        -- ^
+   | VK_NUMPAD2        -- ^
+   | VK_NUMPAD3        -- ^
+   | VK_NUMPAD4        -- ^
+   | VK_NUMPAD5        -- ^
+   | VK_NUMPAD6        -- ^
+   | VK_NUMPAD7        -- ^
+   | VK_NUMPAD8        -- ^
+   | VK_NUMPAD9        -- ^
+   | VK_MULTIPLY       -- ^
+   | VK_ADD            -- ^
+   | VK_SEPARATOR      -- ^
+   | VK_SUBTRACT       -- ^
+   | VK_DECIMAL        -- ^
+   | VK_DIVIDE         -- ^
+   | VK_F1             -- ^
+   | VK_F2             -- ^
+   | VK_F3             -- ^
+   | VK_F4             -- ^
+   | VK_F5             -- ^
+   | VK_F6             -- ^
+   | VK_F7             -- ^
+   | VK_F8             -- ^
+   | VK_F9             -- ^
+   | VK_F10            -- ^
+   | VK_F11            -- ^
+   | VK_F12            -- ^
+   | VK_F13            -- ^
+   | VK_F14            -- ^
+   | VK_F15            -- ^
+   | VK_F16            -- ^
+   | VK_F17            -- ^
+   | VK_F18            -- ^
+   | VK_F19            -- ^
+   | VK_F20            -- ^
+   | VK_F21            -- ^
+   | VK_F22            -- ^
+   | VK_F23            -- ^
+   | VK_F24            -- ^
+
+   | VK_NUMLOCK        -- ^
+   | VK_SCROLL         -- ^
+
+   | VK_OEM_NEC_EQUAL  -- ^
+
+   | VK_OEM_FJ_JISHO   -- ^
+   | VK_OEM_FJ_MASSHOU -- ^
+   | VK_OEM_FJ_TOUROKU -- ^
+   | VK_OEM_FJ_LOYA    -- ^
+   | VK_OEM_FJ_ROYA    -- ^
+
+   | VK_LSHIFT         -- ^
+   | VK_RSHIFT         -- ^
+   | VK_LCONTROL       -- ^
+   | VK_RCONTROL       -- ^
+   | VK_LMENU          -- ^
+   | VK_RMENU          -- ^
+
+   | VK_BROWSER_BACK        -- ^
+   | VK_BROWSER_FORWARD     -- ^
+   | VK_BROWSER_REFRESH     -- ^
+   | VK_BROWSER_STOP        -- ^
+   | VK_BROWSER_SEARCH      -- ^
+   | VK_BROWSER_FAVORITES   -- ^
+   | VK_BROWSER_HOME        -- ^
+
+   | VK_VOLUME_MUTE         -- ^
+   | VK_VOLUME_DOWN         -- ^
+   | VK_VOLUME_UP           -- ^
+   | VK_MEDIA_NEXT_TRACK    -- ^
+   | VK_MEDIA_PREV_TRACK    -- ^
+   | VK_MEDIA_STOP          -- ^
+   | VK_MEDIA_PLAY_PAUSE    -- ^
+   | VK_LAUNCH_MAIL         -- ^
+   | VK_LAUNCH_MEDIA_SELECT -- ^
+   | VK_LAUNCH_APP1         -- ^
+   | VK_LAUNCH_APP2         -- ^
+
+   | VK_OEM_1          -- ^
+   | VK_OEM_PLUS       -- ^
+   | VK_OEM_COMMA      -- ^
+   | VK_OEM_MINUS      -- ^
+   | VK_OEM_PERIOD     -- ^
+   | VK_OEM_2          -- ^
+   | VK_OEM_3          -- ^
+
+   | VK_OEM_4          -- ^
+   | VK_OEM_5          -- ^
+   | VK_OEM_6          -- ^
+   | VK_OEM_7          -- ^
+   | VK_OEM_8          -- ^
+
+   | VK_OEM_AX         -- ^
+   | VK_OEM_102        -- ^
+   | VK_ICO_HELP       -- ^
+   | VK_ICO_00         -- ^
+
+   | VK_PROCESSKEY     -- ^
+
+   | VK_ICO_CLEAR      -- ^
+
+   | VK_PACKET         -- ^
+
+   | VK_OEM_RESET      -- ^
+   | VK_OEM_JUMP       -- ^
+   | VK_OEM_PA1        -- ^
+   | VK_OEM_PA2        -- ^
+   | VK_OEM_PA3        -- ^
+   | VK_OEM_WSCTRL     -- ^
+   | VK_OEM_CUSEL      -- ^
+   | VK_OEM_ATTN       -- ^
+   | VK_OEM_FINISH     -- ^
+   | VK_OEM_COPY       -- ^
+   | VK_OEM_AUTO       -- ^
+   | VK_OEM_ENLW       -- ^
+   | VK_OEM_BACKTAB    -- ^
+
+   | VK_ATTN           -- ^
+   | VK_CRSEL          -- ^
+   | VK_EXSEL          -- ^
+   | VK_EREOF          -- ^
+   | VK_PLAY           -- ^
+   | VK_ZOOM           -- ^
+   | VK_NONAME         -- ^
+   | VK_PA1            -- ^
+   | VK_OEM_CLEAR      -- ^
+
+   deriving (Show,Enum)
+
+data Point = Point
+ { _x :: LONG
+ , _y :: LONG
+ }
+
+{-|
+
+@
+struct POINT
+{
+    LONG  x;
+    LONG  y;
+}
+@
+
+instance Storeable Point where
+  sizeOf _  = sizeOf (0::LONG) + sizeOf (0::LONG)
+
+  -- "The entire structure is aligned on a boundary
+  -- at least as big as the biggest value in the structure"
+  alignment = sizeOf (0::LONG)
+
+  -- peekByteOff address offset = peek (address `plusPtr` offset)
+  peek p = do
+    _x <- peekByteOff p (0 * sizeOf (0::LONG))
+    _y <- peekByteOff p (4 * sizeOf (0::LONG))
+    return Point{..}
+
+  -- pokeByteOff addr off x = poke (addr `plusPtr` off) x
+  poke p Point{..} = do
+    pokeByteOff p (0 * sizeOf (0::LONG)) _x
+    pokeByteOff p (4 * sizeOf (0::LONG)) _y
+    -}
