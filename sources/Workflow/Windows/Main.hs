54a3dc3ce6589a8d07512a70778072d5e73c96b2
Spiros Boosalis <samboosalis@gmail.com>
2016-05-12 11:09:52 +0300

    .

--- sources/Workflow/Windows/Main.hs
+++ sources/Workflow/Windows/Main.hs
@@ -4,21 +4,22 @@ import Workflow.Windows
 
 testWorkflow :: IO ()
 testWorkflow = do
- print =<< getClipboard
+ contents <- getClipboard
+ setClipboard contents
+ print contents
  sendChar 'c'
  sendText "insertText"
- pressKeychord windowsKeyboard [VK_CONTROL] VK_A
+ pressKeychord windowsKeyboard [VK_CONTROL] VK_A -- press "C-a"
  openUrl "http://google.com"
  openApplication "cmd.exe"
  --clickMouseAt windowsMouse (POINT (maxBound `div` 2) minBound) 2 MOUSEEVENTF_LEFTDOWN MOUSEEVENTF_LEFTUP
  clickMouseAt windowsMouse (POINT 800 10) 2 MOUSEEVENTF_LEFTDOWN MOUSEEVENTF_LEFTUP
+ hs_ScrollMouseWheel VerticalWheel Backwards 120
 
 nothing :: IO ()
 nothing = return ()
 
 main :: IO ()
 main = do
- nothing
- hs_ScrollMouseWheel VerticalWheel Backwards 120
- delay 1000
- hs_ScrollMouseWheel VerticalWheel Forwards  120
+ -- nothing
+ testWorkflow
