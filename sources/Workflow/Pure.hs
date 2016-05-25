f1a35e8e4a250f02dc91037f4752500dff8fd85c
Sam Boosalis <samboosalis@gmail.com>
2016-05-25 18:54:29 +0300

    show workflow

--- sources/Workflow/Pure.hs
+++ sources/Workflow/Pure.hs
@@ -1,4 +1,12 @@
+{-| pure interpretations:
+
+* display workflows by patching system variables
+
+-}
+--TODO * run workflows with a crude model of the os
 module Workflow.Pure
  ( module Workflow.Pure.Types
+ , module Workflow.Pure.Execute
  ) where
 import Workflow.Pure.Types
+import Workflow.Pure.Execute
--- sources/Workflow/Pure/Types.hs
+++ sources/Workflow/Pure/Types.hs
@@ -1,4 +1,1 @@
 module Workflow.Pure.Types where
-import Workflow.Pure.Extra()
-
-type WorkflowPure = String
