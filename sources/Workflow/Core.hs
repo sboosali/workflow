b17ea95106bc9c06a0ebe79fde4423f714a01269
Spiros Boosalis <samboosalis@gmail.com>
2016-05-29 01:56:40 +0300

    add .Core

---
+++ sources/Workflow/Core.hs
@@ -0,0 +1,11 @@
+module Workflow.Core
+ ( module Workflow.Types
+ , module Workflow.Execute
+ , module Workflow.Derived
+ , module Workflow.Keys
+ )
+ where
+import Workflow.Types
+import Workflow.Execute
+import Workflow.Derived
+import Workflow.Keys
